83d30719716082d904517da22446eb6339834f05
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2022-12-06 12:15:09 +0100

    Small fix

--- day6/day6.py
+++ day6/day6.py
@@ -1,9 +1,9 @@
 with open('input') as f:
     txt = f.read()
 
-off = 14
+off = 4
 
-for i in range(0, len(txt) - 4):
+for i in range(0, len(txt) - off):
     packet = txt[i : i + off]
     if len(set(packet)) == off:
         print(i + off)
