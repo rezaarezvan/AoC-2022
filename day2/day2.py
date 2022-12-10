41c594a0e1963be30e24717f96261e15151d1c93
rezaarezvan <rezvanr2378@gmail.com>
2022-12-10 13:17:15 +0100

    Day 2; Make a cleaner solution

--- day2/day2.py
+++ day2/day2.py
@@ -1,50 +1,58 @@
-IN = [l.split() for l in open('input')]
+CHOICES = ("S", "P", "R")
 
-R = {"A": 1, "B": 2, "C": 3}
+OPPONENT = {
+    "A": CHOICES[2],
+    "B": CHOICES[1],
+    "C": CHOICES[0]
+}
 
-Q = {"X": 1, "Y": 2, "Z": 3}
+PLAYER = {
+    "X": CHOICES[2],
+    "Y": CHOICES[1],
+    "Z": CHOICES[0]
+}
 
-S = 0
-for a, b in IN:
-    if R[a] == Q[b]:
-        S += 3
-    elif (
-        (a == "A" and b == "Y") or (a == "B" and b == "Z") or (a == "C" and b == "X")
-    ):
-        S += 6
 
-    S += Q[b]
-    
-print(S)
+def get_score(opponent_choice, player_choice):
+    opp_val = CHOICES.index(OPPONENT[opponent_choice])
+    ply_val = CHOICES.index(PLAYER[player_choice])
 
-S = 0
-for a, b in IN:
-    O = None
-    if b == "X":
-        S += 0
-        if a == "A":
-            O = "Z"
-        elif a == "B":
-            O = "X"
-        else:
-            O = "Y"
-    elif b == "Y":
-        S += 3
-        if a == "A":
-            O = "X"
-        elif a == "B":
-            O = "Y"
-        else:
-            O = "Z"
-    else:
-        S += 6
-        if a == "A":
-            O = "Y"
-        elif a == "B":
-            O = "Z"
-        else:
-            O = "X"
-    
-    S += Q[O]
-    
-print(S)
+    result = len(CHOICES) - CHOICES.index(PLAYER[player_choice])
+
+    table = {
+        -1: 6,
+        0: 3,
+        2: 6,
+    }
+
+    return result + table.get((ply_val - opp_val), 0)
+
+
+def main(part=1):
+    score = 0
+    with open("input", "r") as f:
+        rounds = [line.rstrip("\n").split() for line in f.readlines()]
+
+        for round in rounds:
+            if part == 1:
+                score += get_score(round[0], round[1])
+
+            else:
+                player_scores_from_key = list(
+                    map(lambda key: get_score(round[0], key), list(PLAYER.keys())))
+
+                if round[1] == "X":
+                    score += min(player_scores_from_key)
+
+                elif round[1] == "Z":
+                    score += max(player_scores_from_key)
+
+                else:
+                    opponent_choice_to_player = list(
+                        filter(lambda item: item[1] == OPPONENT[round[0]], list(PLAYER.items())))[0][0]
+                    score += get_score(round[0], opponent_choice_to_player)
+
+        return score
+
+print(f'First part: {main()}')
+print(f'Second part: {main(part=2)}')
